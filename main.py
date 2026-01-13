97ba4bf79b8966e4bdf9a478ddc74d76b23ec797
joelsonmarques015-hash <joelsonmarques015@gmail.com>
2026-01-12 23:36:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ if prompt := st.chat_input("Fale com o Joel..."):
         # NÃO ESQUEÇA DE COLOCAR SUA CHAVE ABAIXO
         headers = {"Authorization": "Bearer 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"}
         payload = {
-            "model": "google/gemini-2.0-flash-exp:free",
+            "model": "google/gemini-2.0-pro-exp-02-05:free",
             "messages": [{"role": "system", "content": instrucao}] + st.session_state.messages
         }
 
